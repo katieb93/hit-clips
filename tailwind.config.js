1d9135a726ec83fd5252a7db74db383c6f3e1d18
YOUR NAME <YOUR EMAIL>
2024-07-30 21:48:21 -0500

    seriously

---
+++ tailwind.config.js
@@ -0,0 +1,119 @@
+// // // tailwind.config.js
+// // module.exports = {
+// //   content: [
+// //     "./src/**/*.{js,jsx,ts,tsx}",
+// //   ],
+// //   theme: {
+// //     extend: {
+// //       fontFamily: {
+// //         gunnar: ['Gunnar', 'sans-serif'],
+// //         yearsync: ['"YEAR SYNC"', 'sans-serif'],
+// //       },
+// //       boxShadow: {
+// //         'custom': '-10px 10px 0px 0px #FFD240',
+// //       },
+// //       colors: {
+// //         'custom-pink': '#E6A7D4',
+// //         'custom-yellow': '#FFD240',
+// //         'custom-purple': '#A199D5',
+// //         'custom-dark-purple': '#53498E',
+
+// //       }
+// //     },
+// //   },
+// //   plugins: [],
+// // };
+
+// // tailwind.config.js
+// // module.exports = {
+// //   content: [
+// //     "./src/**/*.{js,jsx,ts,tsx}",
+// //   ],
+// //   theme: {
+// //     extend: {
+// //       fontFamily: {
+// //         gunnar: ['Gunnar', 'sans-serif'],
+// //         yearsync: ['"YEAR SYNC"', 'sans-serif'],
+// //       },
+// //       borderRadius: {
+// //         '30': '30px',
+// //         '25': '25px',
+// //       },
+// //       colors: {
+// //         'custom-yellow': '#FFD240',
+// //         'custom-purple': '#A199D5',
+// //         'custom-dark-purple': '#53498E',
+// //         'custom-pink': '#E6A7D4',
+// //       },
+// //       boxShadow: {
+// //         'custom': '-10px 10px 0px 0px #FFD240',
+// //       },
+// //     },
+// //   },
+// //   plugins: [],
+// // };
+
+// // tailwind.config.js
+// module.exports = {
+//   content: [
+//     "./src/**/*.{js,jsx,ts,tsx}",
+//   ],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         gunnar: ['Gunnar', 'sans-serif'],
+//         yearsync: ['"YEAR SYNC"', 'sans-serif'],
+//       },
+//       borderRadius: {
+//         '30': '30px',
+//         '25': '25px',
+//       },
+//       borderWidth: {
+//         '5': '5px',
+//       },
+//       colors: {
+//         'custom-yellow': '#FFD240',
+//         'custom-purple': '#A199D5',
+//         'custom-dark-purple': '#53498E',
+//         'custom-pink': '#E6A7D4',
+//       },
+//       boxShadow: {
+//         'custom': '-10px 10px 0px 0px #FFD240',
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+
+// tailwind.config.js
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        gunnar: ['Gunnar', 'sans-serif'],
+        yearsync: ['"YEAR SYNC"', 'sans-serif'],
+      },
+      borderRadius: {
+        '30': '30px',
+        '25': '25px',
+      },
+      borderWidth: {
+        '5': '5px',
+      },
+      colors: {
+        'custom-yellow': '#FFD240',
+        'custom-purple': '#A199D5',
+        'custom-dark-purple': '#53498E',
+        'custom-pink': '#E6A7D4',
+      },
+      boxShadow: {
+        'custom': '-10px 10px 0px 0px #FFD240',
+      },
+    },
+  },
+  plugins: [],
+};
